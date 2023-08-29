9c0fd7b3aa0180c0eca6a032ae55547660d3ebdd
Vũ Tiến Quốc <42103573+Kedoichi@users.noreply.github.com>
2023-08-29 23:55:11 +1000

    Add search box

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,35 +1,62 @@
 import 'package:flutter/material.dart';
 import 'package:todoapp/constrants/colors.dart';
 
-class Home extends StatelessWidget{
+class Home extends StatelessWidget {
   const Home({Key? key}) : super(key: key);
 
   @override
-  Widget build(BuildContext context){
+  Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      backgroundColor: tdBGColor,
+      appBar: _appBar(),
+      body: Container(
+        padding: EdgeInsets.symmetric(horizontal: 20,vertical: 15),
+        child: Column(
           children: [
-          Icon(Icons.menu,color: tdBlack,size: 30,),
+            searchBox()
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget searchBox() {
+    return Container(
+        padding: EdgeInsets.symmetric(horizontal: 10),
+        decoration: BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.circular(20),
+        ),
+        child: TextField(
+          decoration: InputDecoration(
+            border: InputBorder.none,
+            hintText: 'Search',
+            prefixIcon: Icon(Icons.search),
+          ),
+        ));
+  }
+
+  AppBar _appBar() {
+    return AppBar(
+      title: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Icon(
+            Icons.menu,
+            color: tdBlack,
+            size: 30,
+          ),
           Container(
               height: 40,
               width: 40,
               child: ClipRRect(
                 borderRadius: BorderRadius.circular(20),
                 child: Image.asset('assets/images/avatar.png'),
-              )
-          ),
+              )),
         ],
-        ),
-        backgroundColor: tdBGColor,
       ),
-      body: const Center(
-        child: Text(
-          'Home',
-          style: TextStyle(fontSize: 24),
-        ),
-      ),
+      backgroundColor: tdBGColor,
+      elevation: 0,
     );
   }
 }
